786583bb90732a1cf4c0a8db54fed3bc479ea94d
flaxonpro <56710806+flaxonpro@users.noreply.github.com>
2024-01-25 16:02:16 -0500

    Add version 4.2.0

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = 'directfolders'
+  fileType      = 'exe'
+  url           = 'https://codesector.com/files/directfolders4.2.exe'
+  softwareName  = 'Direct Folders'
+  checksum      = '7BA467E5CEE6D7D82B2719FC66CBAFDD2D1DD0D72DC4D5E79AEBFF81873D1AB6'
+  checksumType  = 'sha256'
+  silentArgs    = "/exenoui /exenoupdates /qn /norestart"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
